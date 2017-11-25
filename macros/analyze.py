3cc871d99632fe022b30b0fd389e8eca8c555010
Devdatta Majumder <devdatta.majumder@cern.ch>
2017-11-25 13:58:41 +0100

    adding Mjj for no vbf sel+Htag

--- macros/analyze.py
+++ macros/analyze.py
@@ -20,38 +20,43 @@ files  = sys.argv[1]
 fout = ROOT.TFile(files.rstrip().replace('txt', 'root'), 'RECREATE')
 fout.cd()
 
-h2_ak8pt_ak8eta       = ROOT.TH2D("h2_ak8pt_ak8eta"        , ";p_{T} (AK8) [GeV]; #eta (AK8);;", 150, 0., 3000, 50, -5, 5)
-h2_ak4pt_ak4eta       = ROOT.TH2D("h2_ak4pt_ak4eta"        , ";p_{T} (AK4) [GeV]; #eta (AK4);;", 150, 0., 3000, 50, -5, 5)
-h2_ak4pt_ak4genjetpt  = ROOT.TH2D("h2_ak4pt_ak4genjetpt"   , ";p_{T} (AK4 jets) [GeV]; p_{T} (AK4 genjets) [GeV];Events;" ,300,0.,3000,300,0.,3000)
+h2_ak8pt_ak8eta          = ROOT.TH2D("h2_ak8pt_ak8eta"         ,";p_{T} (AK8) [GeV]; #eta (AK8);;", 150, 0., 3000, 50, -5, 5)
+h2_ak8pt_ak8eta_htagged  = ROOT.TH2D("h2_ak8pt_ak8eta_htagged" ,";p_{T} (AK8) [GeV]; #eta (AK8);;", 150, 0., 3000, 50, -5, 5)
 
-h_nak8           = ROOT.TH1D("h_nak8"            , ";N(AK8) [GeV]; Events;;"      ,11   ,-0.5  ,10.5 )
-h_nak4           = ROOT.TH1D("h_nak4"            , ";N(AK4) [GeV]; Events;;"      ,501  ,-0.5  ,501.5)
+h2_ak4pt_ak4eta          = ROOT.TH2D("h2_ak4pt_ak4eta"         ,";p_{T} (AK4) [GeV]; #eta (AK4);;", 150, 0., 3000, 50, -5, 5)
+h2_ak4pt_ak4genjetpt     = ROOT.TH2D("h2_ak4pt_ak4genjetpt"    ,";p_{T} (AK4 jets) [GeV]; p_{T} (AK4 genjets) [GeV];Events;" ,300,0.,3000,300,0.,3000)
 
-h_ak80pt         = ROOT.TH1D("h_ak80pt"          , ";p_{T} [GeV]; Events;;"       ,300  ,0.    ,3000 )
-h_ak81pt         = ROOT.TH1D("h_ak81pt"          , ";p_{T} [GeV]; Events;;"       ,300  ,0.    ,3000 )
-h_ak80eta        = ROOT.TH1D("h_ak80eta"         ,";#eta;;"                       ,200  ,-5    ,5    )
-h_ak81eta        = ROOT.TH1D("h_ak81eta"         ,";#eta;;"                       ,200  ,-5    ,5    )
-h_ak80_tau2_tau1 = ROOT.TH1D("h_ak80_t2byt1"     ,";#tau_{2}/#tau_{1};;"          ,100  ,0     ,1    )
-h_ak81_tau2_tau1 = ROOT.TH1D("h_ak81_t2byt1"     ,";#tau_{2}/#tau_{1} ;;"         ,100  ,0     ,1    )
-h_sdmass_ak80    = ROOT.TH1D("h_sdmass_ak80"     ,";softdropped_mass[GeV];;"      ,100  ,0     ,1000 )
-h_sdmass_ak81    = ROOT.TH1D("h_sdmass_ak81"     ,";softdropped_mass[GeV];;"      ,100  ,0     ,1000 )
+h_nak8                   = ROOT.TH1D("h_nak8"                  ,";N(AK8) [GeV]; Events;;"       ,11   ,-0.5  ,10.5 )
+h_nak4                   = ROOT.TH1D("h_nak4"                  ,";N(AK4) [GeV]; Events;;"       ,501  ,-0.5  ,501.5)
 
-h_nvbfpairs      = ROOT.TH1D("h_nvbfpairs"       , ";N(VBF pairs); Events;;"      ,201  ,-0.5  ,200.5 )
-h_vbf0pt         = ROOT.TH1D("h_vbf0pt"          , ";p_{T} (VBF) [GeV]; Events;;" ,100  ,0.    ,1000 )
-h_vbf1pt         = ROOT.TH1D("h_vbf1pt"          , ";p_{T} (VBF) [GeV]; Events;;" ,100  ,0.    ,1000 )
-h_vbf0eta        = ROOT.TH1D("h_vbf0eta"         ,";#eta (VBF); Events;"          ,200  ,-5    ,5    )
-h_vbf1eta        = ROOT.TH1D("h_vbf1eta"         ,";#eta (VBF); Events;"          ,200  ,-5    ,5    )
-h_deltaEta       = ROOT.TH1D("h_deltaEta"        ,";#delta#eta of VBF jets;;"     ,20   ,0     ,10   )
-h_mjjvbf         = ROOT.TH1D("h_mjjvbf"          , ";M(jj) (VBF) [GeV]; Events;"  ,200  ,0.    ,2000 )
+h_ak80pt                 = ROOT.TH1D("h_ak80pt"                ,";p_{T} [GeV]; Events;;"        ,300  ,0.    ,3000 )
+h_ak81pt                 = ROOT.TH1D("h_ak81pt"                ,";p_{T} [GeV]; Events;;"        ,300  ,0.    ,3000 )
+h_ak80eta                = ROOT.TH1D("h_ak80eta"               ,";#eta;;"                       ,200  ,-5    ,5    )
+h_ak81eta                = ROOT.TH1D("h_ak81eta"               ,";#eta;;"                       ,200  ,-5    ,5    )
+h_ak80_tau2_tau1         = ROOT.TH1D("h_ak80_t2byt1"           ,";#tau_{2}/#tau_{1};;"          ,100  ,0     ,1    )
+h_ak81_tau2_tau1         = ROOT.TH1D("h_ak81_t2byt1"           ,";#tau_{2}/#tau_{1} ;;"         ,100  ,0     ,1    )
+h_sdmass_ak80            = ROOT.TH1D("h_sdmass_ak80"           ,";softdropped_mass[GeV];;"      ,100  ,0     ,1000 )
+h_sdmass_ak81            = ROOT.TH1D("h_sdmass_ak81"           ,";softdropped_mass[GeV];;"      ,100  ,0     ,1000 )
 
-h_sj0_pts        = ROOT.TH1D("h_sj0_pts"         ,";p_{T} of subjet_1;;"          ,300   ,0     ,3000 )
-h_sj1_pts        = ROOT.TH1D("h_sj1_pts"         ,";p_{T} of subjet_2;;"          ,300   ,0     ,3000 )
-h_sj0_csvv2      = ROOT.TH1D("h_sj0_csvv2"       ,";CSVv2 of subjet_1;;"          ,100   ,0.    ,1.   )
-h_sj1_csvv2      = ROOT.TH1D("h_sj1_csvv2"       ,";CSVv2 of subjet_2;;"          ,100   ,0.    ,1.   )
-h_sj0_deepcsv    = ROOT.TH1D("h_sj0_deepcsv"     ,";DeepCSV of subjet_1;;"        ,100   ,0.    ,1.   )
-h_sj1_deepcsv    = ROOT.TH1D("h_sj1_deepcsv"     ,";DeepCSV of subjet_2;;"        ,100   ,0.    ,1.   )
+h_nvbfpairs              = ROOT.TH1D("h_nvbfpairs"             ,";N(VBF pairs); Events;;"       ,201  ,-0.5  ,200.5 )
+h_vbf0pt                 = ROOT.TH1D("h_vbf0pt"                ,";p_{T} (VBF) [GeV]; Events;;"  ,100  ,0.    ,1000 )
+h_vbf1pt                 = ROOT.TH1D("h_vbf1pt"                ,";p_{T} (VBF) [GeV]; Events;;"  ,100  ,0.    ,1000 )
+h_vbf0eta                = ROOT.TH1D("h_vbf0eta"               ,";#eta (VBF); Events;"          ,200  ,-5    ,5    )
+h_vbf1eta                = ROOT.TH1D("h_vbf1eta"               ,";#eta (VBF); Events;"          ,200  ,-5    ,5    )
+h_deltaEta               = ROOT.TH1D("h_deltaEta"              ,";#delta#eta of VBF jets;;"     ,20   ,0     ,10   )
+h_mjjvbf                 = ROOT.TH1D("h_mjjvbf"                ,";M(jj) (VBF) [GeV]; Events;"   ,200  ,0.    ,2000 )
 
-h_mjj            = ROOT.TH1D("h_mjj"             , ";M(jj) [GeV]; Events;"        ,400  ,500.   ,4500 )
+h_sj0_pts                = ROOT.TH1D("h_sj0_pts"               ,";p_{T} of subjet_1;;"          ,300   ,0     ,3000 )
+h_sj1_pts                = ROOT.TH1D("h_sj1_pts"               ,";p_{T} of subjet_2;;"          ,300   ,0     ,3000 )
+h_sj0_csvv2              = ROOT.TH1D("h_sj0_csvv2"             ,";CSVv2 of subjet_1;;"          ,100   ,0.    ,1.   )
+h_sj1_csvv2              = ROOT.TH1D("h_sj1_csvv2"             ,";CSVv2 of subjet_2;;"          ,100   ,0.    ,1.   )
+h_sj0_deepcsv            = ROOT.TH1D("h_sj0_deepcsv"           ,";DeepCSV of subjet_1;;"        ,100   ,0.    ,1.   )
+h_sj1_deepcsv            = ROOT.TH1D("h_sj1_deepcsv"           ,";DeepCSV of subjet_2;;"        ,100   ,0.    ,1.   )
+
+h_mjj_vbfsel             = ROOT.TH1D("h_mjj_vbfsel"            ,";M(jj) [GeV]; Events;"         ,400  ,500.   ,4500 )
+h_mjj_novbf_nosjbtag     = ROOT.TH1D("h_mjj_novbf_nosjbtag"    ,";M(jj) [GeV]; Events;"         ,400  ,500.   ,4500 )
+h_mjj_novbf_2sjbtag      = ROOT.TH1D("h_mjj_novbf_2sjbtag"     ,";M(jj) [GeV]; Events;"         ,400  ,500.   ,4500 )
+h_mjj                    = ROOT.TH1D("h_mjj"                   ,";M(jj) [GeV]; Events;"         ,400  ,500.   ,4500 )
 
 fnames = [line.strip() for line in open(files, 'r')]
 
@@ -109,12 +114,18 @@ for fname in fnames:
     h_nak4.Fill(nak4)
 
     ### Higgs jet sel:
+
     ptsel      = pts[0] > 300 and pts[1] > 300
     etasel     = abs(etas[0]) < 3 and abs(etas[1]) < 3 
     msdsel     = 80 < sd_masses[0] < 160 and 60 < sd_masses[1] < 140
     tau21sel   = tau2s[0]/tau1s[0] < 0.6 and tau2s[1]/tau1s[1] < 0.6
     sjbtagsel  = deepcsv_sj0s[0] > 0.6324 and deepcsv_sj1s[0] > 0.6324  and deepcsv_sj0s[1] > 0.6324 and deepcsv_sj1s[1] > 0.6324 
     hjetssel   = ptsel and etasel and msdsel and tau21sel and sjbtagsel
+
+    sjbtag_1 = deepcsv_sj0s[0] > 0.6324 or deepcsv_sj1s[0] > 0.6324  or deepcsv_sj0s[1] > 0.6324 or deepcsv_sj1s[1] > 0.6324
+    sjbtag_2 = (deepcsv_sj0s[0] > 0.6324 or deepcsv_sj1s[0] > 0.6324) and (deepcsv_sj0s[1] > 0.6324 or deepcsv_sj1s[1] > 0.6324) 
+    sjbtag_3 = ( (deepcsv_sj0s[0] > 0.6324 and deepcsv_sj1s[0] > 0.6324) and (deepcsv_sj0s[1] > 0.6324 or deepcsv_sj1s[1] > 0.6324) ) or\
+        ( (deepcsv_sj0s[0] > 0.6324 or deepcsv_sj1s[0] > 0.6324) and (deepcsv_sj0s[1] > 0.6324 and deepcsv_sj1s[1] > 0.6324) )
 
     ### Select AK4 jets for VBF pair identification
     p4_ak4sel = []
@@ -156,22 +167,45 @@ for fname in fnames:
 
     h2_ak8pt_ak8eta.Fill(pts[0], etas[0])
     h2_ak8pt_ak8eta.Fill(pts[1], etas[1])
+   
+    ### Jet 1 passing Higgs tagging:
+    if 80 < sd_masses[0] < 160 and tau2s[0]/tau1s[0] < 0.6 and deepcsv_sj0s[0] > 0.6324 and deepcsv_sj1s[0] > 0.6324:
+      h2_ak8pt_ak8eta_htagged.Fill(pts[0], etas[0])
+    ### Jet 2 passing Higgs tagging:
+    if 80 < sd_masses[1] < 160 and tau2s[1]/tau1s[1] < 0.6 and deepcsv_sj0s[1] > 0.6324 and deepcsv_sj1s[1] > 0.6324:
+      h2_ak8pt_ak8eta_htagged.Fill(pts[1], etas[1])
 
     ### Fill Higgs jet quantities after VBF selection:
+    p4_ak80 = ROOT.TLorentzVector()
+    p4_ak81 = ROOT.TLorentzVector()
+    p4_ak80.SetPtEtaPhiM(pts[0], etas[0], phis[0], masses[0])
+    p4_ak81.SetPtEtaPhiM(pts[1], etas[1], phis[1], masses[1])
+    mjj = (p4_ak80 + p4_ak81).Mag()
+
+    if ptsel and etasel and msdsel and tau21sel:
+      h_mjj_novbf_nosjbtag.Fill(mjj)
+
     if vbfsel:
+      h_mjj_vbfsel.Fill(mjj)
+
       if etasel and msdsel and tau21sel and sjbtagsel:
         h_ak80pt.Fill(pts[0])
         h_ak81pt.Fill(pts[1])
+
       if ptsel and msdsel and tau21sel and sjbtagsel:
         h_ak80eta.Fill(etas[0])
         h_ak81eta.Fill(etas[1])
+
       if ptsel and etasel and tau21sel and sjbtagsel:
         h_sdmass_ak80.Fill(sd_masses[0])
         h_sdmass_ak81.Fill(sd_masses[1])
+
       if ptsel and etasel and msdsel and sjbtagsel:
         h_ak80_tau2_tau1.Fill(tau2s[0]/tau1s[0])
         h_ak81_tau2_tau1.Fill(tau2s[1]/tau1s[1])
+
       if ptsel and etasel and msdsel and tau21sel:
+        h_mjj_novbf_nosjbtag.Fill(mjj)
         h_sj0_pts.Fill(sj0_pts[0]) 
         h_sj0_pts.Fill(sj0_pts[1]) 
         h_sj1_pts.Fill(sj1_pts[0]) 
@@ -185,13 +219,11 @@ for fname in fnames:
         h_sj1_deepcsv.Fill(deepcsv_sj1s[0]) 
         h_sj1_deepcsv.Fill(deepcsv_sj1s[1]) 
 
+      if ptsel and etasel and msdsel and tau21sel and sjbtag_2:
+        h_mjj_novbf_2sjbtag.Fill(mjj)
+
     ### Full event selection
     if hjetssel and vbfsel:
-      p4_ak80 = ROOT.TLorentzVector()
-      p4_ak81 = ROOT.TLorentzVector()
-      p4_ak80.SetPtEtaPhiM(pts[0], etas[0], phis[0], masses[0])
-      p4_ak81.SetPtEtaPhiM(pts[1], etas[1], phis[1], masses[1])
-      mjj = (p4_ak80 + p4_ak81).Mag()
       h_mjj.Fill(mjj)
                                  
     ievt += 1
